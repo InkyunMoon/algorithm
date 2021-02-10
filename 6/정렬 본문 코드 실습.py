0ccf4eb260fdabf8fae58d9e43810b87828c192d
InkyunMoon <mikinkyun@gmail.com>
2021-02-10 10:03:57 +0900

    0210 temporary save

--- 6/정렬 본문 코드 실습.py
+++ 6/정렬 본문 코드 실습.py
@@ -31,7 +31,6 @@ def quick_sort(array, start, end):
     left = start + 1
     right = end
     while left <= right:
-
         while left <= end and array[left] <= array[pivot]:
             left += 1
         while right > start and array[right] >= array[pivot]:
@@ -40,6 +39,7 @@ def quick_sort(array, start, end):
             array[right], array[pivot] = array[pivot], array[right]
         else:
             array[left], array[right] = array[right], array[left]
+
     quick_sort(array, start, right - 1)
     quick_sort(array, right + 1, end)
 
