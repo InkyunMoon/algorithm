f53f556ce1abaaadc8afdcf1a63fe1913f01c3b2
InkyunMoon <ik.moon23@gmail.com>
2021-02-20 18:48:51 +0900

    효율적인 화폐구성 진행중

--- 8_dynamic programming/실전문제4-효율적인 화폐 구성.py
+++ 8_dynamic programming/실전문제4-효율적인 화폐 구성.py
@@ -2,7 +2,20 @@
 # 개수의 최소한 합으로 가치가 M원이 되도록
 
 n, m = list(map(int,input().split()))
+# n, m = 2, 15
 
-v = []
+array = []
 for i in range(n):
-    v.append(int(input()))
+    array.append(int(input()))
+
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1): # j는 각 화폐를 나타낸다.
+        # j == 15까지 반복문 수행
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j-array[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
