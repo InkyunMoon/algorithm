b516b21576947edc6320989862ef4b13a5e38107
inkyunmoon <ik.moon23@gmail.com>
2021-02-23 22:13:42 +0900

    개선된 다익스트라 알고리즘 설명 수정

--- 9_shortest_path/본문실습.py
+++ 9_shortest_path/본문실습.py
@@ -67,21 +67,25 @@ for _ in range(m):
 
 def dijkstra(start):
     q = []
-    heapq.heappush(q, (0, start)) # 출발노드로 가는 거리를 0으로 설정해서 우선순위큐에 넣어준다.
-    distance[start] = 0 # 출발노드(start)로 가는 거리를 0으로 설정하고 큐에 넣도록 한다.
+    heapq.heappush(q, (0, start)) # q라는 힙에 (거리:0, 시작지점:start)인 튜플을 삽입. 출발노드로 가는 거리를 0으로 설정해서 우선순위큐에 넣어준다.
+    distance[start] = 0 # 시작지점으로까지의 거리는 0으로 초기화. 출발노드(start)로 가는 거리를 0으로 설정하고 큐에 넣도록 한다.
     while q: # 우선순위 큐가 빌 때까지 데이터를 하나씩 꺼내서 처리하도록 한다.
-        dist, now = heapq.heappop(q) # dist가 작은 순서부터 우선하여 꺼내진다.
-        if distance[now] < dist: # 꺼내진 노드가 현재 거리보다 작으면 (처리된 것으로 간주하고)무시하도록 한다.
+        dist, now = heapq.heappop(q) # 거리, 현재 지점을 꺼낸다. dist가 작은 순서부터 우선하여 꺼내진다.
+        if distance[now] < dist: # 현재 노드로 가는 거리가 최단거리테이블의 거리보다 작으면 무시한다. 꺼내진 노드가 현재 거리보다 작으면 (처리된 것으로 간주하고)무시하도록 한다.
+            # dist는 현재 꺼낸 노드에서 특정 인접노드로 가는 거리
             continue
-        for i in graph[now]: # 현재 꺼낸 노드에 대해서 그 노드를 거쳐가는 경우를 고려하도록 한다.
+        # 현재 노드와 인접한 노드들 확인
+        for i in graph[now]: # 현재 꺼낸 노드까지 도달하는 거리 + 인접한 노드들까지 가는 거리. 현재 꺼낸 노드에 대해서 그 노드를 거쳐가는 경우를 고려하도록 한다.
             cost = dist + i[1] # i[1]는 현재 꺼낸 노드와 인접한 노드의 거리를 의미한다.
+            # i는 현재 노드 now로부터 (인접노드, 가는 비용)으로 구성되어있다.
             if cost < distance[i[0]]: # 현재 노드를 거쳐서 다른노드로 이동하는 거리가 짧은 경우에 다음의 코드 실행
                 distance[i[0]] = cost # 그 작은 비용으로 갱신한다.
+                # 즉, 그렇게 구한 거리가 인접한 노드까지의 최단거리보다 짧으면 최단거리 테이블을 갱신한다.
                 heapq.heappush(q, (cost, i[0])) # 그럴 때마다 우선순위큐에 해당 정보가 기록되도록 한다.
-
+                # 다시말해, 우선순위 큐에 해당 노드로 가는 비용을 업데이트하는 것.
 dijkstra(start)
 
-for i in range(1, n+1):
+for i in range(1, n+1): # 모든 노드로 가기 위한 최단 거리 출력
     if distance[i] == INF:
         print('INFINITY') # 도달할 수 없는 경우, 무한으로 출력
     else:
